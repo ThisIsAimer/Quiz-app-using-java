0775111cf04803a9fc38803d632494fd76cca48e
raunak <raunakbiswas05@gmail.com>
2024-11-13 20:33:02 +0530

    Unfinished quiz app

---
+++ QuestionService.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class QuestionService {
+    Questions question[] = new Questions[5];
+    Scanner input = new Scanner(System.in);
+    
+
+        void start() {
+        System.out.println("Welcome to this quiz game! Press enter to continue: ");
+        String i = input.nextLine();
+        i = "Lets go!";
+        System.err.println(i);
+        }
+
+        void game() {
+            for(int i =0;i<5;i++){
+                question[i].setId(i+1);
+            }
+            //setting the questions
+            question[0].setQuestions("Who is the current prime minister (2024) of india?");
+            question[1].setQuestions("What is the name of our planet?");
+            question[2].setQuestions("In which country does anime originate?");
+            question[3].setQuestions("Whats the name of the currency used in US?");
+            question[4].setQuestions("Whats the GDP of india?");
+
+            // options
+            question[0].setOptions("A.Rahul Gandhi", "B.Narandra Modi", "C.Mahatma Gandhi", "D.APJ Abdulkalam");
+            question[1].setOptions("A.Moon", "B.Sun", "C.Earth", "D.Mars");
+            question[2].setOptions("A.Japan", "B.France", "C.Afganistan", "D.Russia");
+            question[3].setOptions("A.Indian Rupee", "B.Japanese Yen", "C.Us Dollar", "D.Euro");
+            question[4].setOptions("A.10.56 trillion", "B.2.54 trillion", "C.6.87 trillion", "D.3.89 trillion");
+            // set answers
+            question[0].setAnswer("Narandra Modi");
+            question[0].setAnswerOption("B");
+            question[1].setAnswer("Earth");
+            question[1].setAnswerOption("C");
+            question[2].setAnswer("Japan");
+            question[2].setAnswerOption("A");
+            question[3].setAnswer("Us Dollar");
+            question[3].setAnswerOption("C");
+            question[4].setAnswer("3.89 trillion");
+            question[4].setAnswerOption("D");
+
+            // function starts!
+            for(int i = 0; i<5;i++){
+                System.out.print(question[i].getId()+" "+question[i].getQuestions());
+                System.out.println("--------------------------------------------------");
+                for(int j = 0; j<4;j++){
+                    System.out.print(question[i].getOptions[j]" ");
+                }
+
+            }
+        }
+        
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Questions.java
@@ -0,0 +1,68 @@
+public class Questions {
+    private int id;
+    private String questions;
+    private String options[] = new String[4];
+    private String selectedAnswer;
+    private String answer;
+    private String answerOption;
+
+
+    public Questions() {
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+    public String getQuestions() {
+        return questions;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public String getSelectedAnswer() {
+        return selectedAnswer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+    
+    public String getAnswerOption() {
+        return answerOption;
+    }
+
+    
+//---------------------------------------------------------
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setQuestions(String questions) {
+        this.questions = questions;
+    }
+
+    public void setOptions(String option1,String option2, String option3, String option4) {
+        int i = 0;
+        String parameters[] = {option1,option2,option3,option4};
+        for (String item : parameters){
+            options[i] = item;
+            i++;
+        }
+    }
+
+    public void setSelectedAnswer(String selectedAnswer) {
+        this.selectedAnswer = selectedAnswer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setAnswerOption(String answerOption) {
+        this.answerOption = answerOption;
+    }
+
+}
